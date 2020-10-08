181c0458e7165d33f06a0fac4f077367dc52933c
N-1-K-k-1 <N1Kk1.pro.123@gmail.com>
2020-10-08 12:54:08 +0300

    initial

---
+++ app/src/main/java/com/N1Kk1/crazybirdy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.N1Kk1.crazybirdy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
